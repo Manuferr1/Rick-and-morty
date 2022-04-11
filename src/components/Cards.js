62aaa75529bf13c9fd79656754840d4de2668101
Manuel Ferreira <manuel.ferreira@snackpass.co>
2022-04-11 12:10:03 +0200

    Added Pagination, and Navbar, fail in search

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,30 +1,29 @@
 // import React from 'react' no es necesario (comando rfc)
-import React from 'react'
+import React from "react";
 
-const Cards = ({results}) => {
-  
-  let display; 
-  
-    if(results){ 
-      display = results.map((x) => {  
-        let {id , name, image, origin} = x
-         return (
-          <div key={id} className="col-4"> {/*Al devolver col-4, cada imagen ocupa 4 columnas (de 12)*/}
-           <img src={image} alt="" className="img-fluid" />
-           <div className="content">
-            <p className='fs-5'>{name}</p>
-            <p className='fs-7'>Origen: {origin.name}</p>
-            
-           </div>
+const Cards = ({ results }) => {
+  let display;
+
+  if (results) {
+    display = results.map((x) => {
+      let { id, name, image, origin } = x;
+      return (
+        <div key={id} className="col-3">
+          {" "}
+          {/*Al devolver col-4, cada imagen ocupa 4 columnas (de 12)*/}
+          <img src={image} alt="" className="img-fluid" />
+          <div className="content">
+            <p className="fs-5">{name}</p>
+            <p className="fs-7">Origen: {origin.name}</p>
           </div>
-         ) 
-          
-        });  
-    }else{
-      display = "No  characters found"
-    }
-    
-    return <>{display}</> 
-}
+        </div>
+      );
+    });
+  } else {
+    display = "No  characters found";
+  }
 
-export default Cards
+  return <>{display}</>;
+};
+
+export default Cards;
